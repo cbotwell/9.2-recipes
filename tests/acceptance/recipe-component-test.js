95525b5ba587f3f72564a0ee8aae996ceee1e4ae
Corbin Otwell <cbotwell@gmail.com>
2015-08-30 20:36:33 -0500

    adds a test for seeing a recipe

--- tests/acceptance/recipe-component-test.js
+++ tests/acceptance/recipe-component-test.js
@@ -30,10 +30,26 @@ test('A user should see a recipe card', function(assert) {
     var ingredients = findWithAssert('.ingredients');
     var directions = findWithAssert('.directions');
 
-    assert.equal(card, true);
-    assert.equal(header, true);
-    assert.equal(servings, true);
-    assert.equal(ingredients, true);
-    assert.equal(directions, true);
+    assert.ok(card);
+    assert.ok(header);
+    assert.ok(servings);
+    assert.ok(ingredients);
+    assert.ok(directions);
+  });
+});
+
+test('A user should see a recipe', function(assert) {
+  visit('/');
+
+  andThen(function() {
+    var recipeName = findWithAssert('.header').html();
+    var servings = findWithAssert('.servings').html();
+    var firstIngredient = findWithAssert('.ingredients li:eq(0)').html();
+    var directions = findWithAssert('.directions').html();
+
+    assert.equal(recipeName, 'The Best Ceasar');
+    assert.equal(servings, 'Makes 4 Servings');
+    assert.equal(firstIngredient, '2 medium heads of romain lettuce');
+    assert.equal(directions, 'Add a pinch of salt and using a fork, mash anchovies. Add lemon juice and red wine vinegar and whisk until anchovies are broken down. Add freshly peeled garlic cloves, Add Grey Poupon, Tabasco, Worchestershire sauce, black pepper, evoo, and whisk until thickly blended. Added chilled lettuce leaves torn to bite size peices and toss by hand. Top with parmesan cheese and season with black pepper.');
   });
 });
